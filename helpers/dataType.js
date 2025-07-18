4a5ef8e672c15d6c04699f2084aecfa7cbe8cadb
longt2407 <longt2407@gmail.com>
2025-07-17 20:37:28 -0500

    add dataType.js and validator.js

---
+++ helpers/dataType.js
@@ -0,0 +1,132 @@
+import utils from "./utils.js";
+
+class ARRAY {
+    check(val) {
+        if (val && !Array.isArray(val)) {
+            return {
+                error: new Error("invalid")
+            }; 
+        }
+        if (val) {
+            for (let v of val) {
+                if(!this.elementType.check(v)) {
+                    return {
+                        error: new Error("invalid")
+                    };
+                }
+            }
+        }
+        return {};
+    }
+
+    constructor(elementType) {
+        this.elementType = elementType;
+    }
+}
+
+class NUMBER {
+    check(val) {
+        if (val && utils.isNaN(val)) {
+            return {
+                error: new Error("invalid")
+            };
+        }
+        if (val && this.opt.check && this.opt.check(val) && this.opt.check(val).error) {
+            return {
+                error: new Error("invalid")
+            };
+        }
+        return {};
+    }
+
+    constructor(opt = {}) {
+        this.opt = utils.objectAssign(["check"], opt);
+    }
+}
+
+class STRING {
+    check(val) {
+        if (val && typeof val !== "string") {
+            return {
+                error: new Error("invalid")
+            };
+        }
+        if (val && this.opt.check && this.opt.check(val) && this.opt.check(val).error) {
+            return {
+                error: new Error("invalid")
+            };
+        }
+        return {};
+    }
+    
+    constructor(opt = {}) {
+        this.opt = utils.objectAssign(["check"], opt);
+    }
+}
+
+class TIMESTAMP {
+    check(val) {
+        // val must be in milliseconds
+        if (val && utils.isNaN(val)) {
+            return {
+                error: new Error("invalid")
+            };
+        }
+        if (val && this.opt.check && this.opt.check(val) && this.opt.check(val).error) {
+            return {
+                error: new Error("invalid")
+            };
+        }
+        return {};
+    }
+
+    constructor(opt = {}) {
+        this.opt = utils.objectAssign(["check"], opt);
+    }
+}
+
+class ANY {
+    check(val) {
+        return {};
+    }
+    constructor() {}
+}
+
+class NOTNULL {
+    check(val) {
+        if (val === null) {
+            return {
+                error: new Error("invalid")
+            };
+        }
+        if (val === undefined) {
+            return {
+                error: new Error("invalid")
+            };
+        }
+        if (typeof val === "string" && val.trim() === "") {
+            return {
+                error: new Error("invalid")
+            };
+        }
+        if (Array.isArray(val) && val.length === 0) {
+            return {
+                error: new Error("invalid")
+            };
+        }
+        return {};
+    }
+
+    constructor() {}
+}
+
+class DataType {
+    static ARRAY(opt) { return new ARRAY(opt); }
+    static NUMBER(opt) { return new NUMBER(opt); }
+    static STRING(opt) { return new STRING(opt); }
+    static TIMESTAMP(opt) { return new TIMESTAMP(opt); }
+    static ANY(opt) { return new ANY(opt); }
+    static NOTNULL(opt) { return new NOTNULL(opt); }
+}
+
+export default DataType;
---
+++ helpers/validator.js
@@ -0,0 +1,62 @@
+import { HttpError } from "../helpers/error.js";
+
+// import DataType from "./dataType.js";
+// const validator = new Validator({
+//     x: [DataType.NUMBER()],
+//     y: [DataType.NUMBER(), DataType.NOTNULL()],
+//     z: [
+//             DataType.NUMBER({ check: (val) => { 
+//                 if (val !== 0 && val !== 1) {
+//                     return { error: new Error("invalid") };
+//                 }
+//             }}), 
+//             DataType.NOTNULL()
+//         ],
+//     a: [DataType.STRING()],
+//     b: [DataType.STRING(), DataType.NOTNULL()],
+//     c: [
+//             DataType.STRING({ check: (val) => {
+//                 if (!/^.+@.+$/.test(val)) {
+//                     return { error: new Error("invalid") };
+//                 }
+//             }}), 
+//             DataType.NOTNULL()
+//         ]
+// });
+class Validator {
+    validate(row) {
+        for (let attr in this.config) {
+            if (attr in row) {
+                for (let dataType of this.config[attr]) {
+                    let result = dataType.check(row[attr]);
+                    if(result.error) {
+                        throw new HttpError({ statusCode: 400, message: `${attr} is invalid.` });
+                    }
+                }
+            }
+        }
+    }
+
+    check(row) {
+        for (let attr in this.config) {
+            if (attr in row) {
+                for (let dataType of this.config[attr]) {
+                    let result = dataType.check(row[attr]);
+                    if(result.error) {
+                        return {
+                            attr,
+                            error: result.error
+                        };
+                    }
+                }
+            }
+        }
+        return {};
+    }
+
+    constructor(config) {
+        this.config = config;
+    }
+}
+
+export default Validator;
