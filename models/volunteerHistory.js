a41975e6aa265c49664b3a4a46fb5b98d2fa60b0
longt2407 <longt2407@gmail.com>
2025-08-04 05:05:27 -0500

    modify volunteer history api

--- models/volunteerHistory.js
+++ models/volunteerHistory.js
@@ -3,7 +3,8 @@ import db from '../controllers/db.js';
 export const getHistoryByVolunteerId = async (conn, volunteer_id) => {
    //console.log('Fetching history for volunteer_id:', volunteer_id);
     const [rows] = await conn.query(`
-        SELECT ve.id, e.name AS event_name, ve.status AS type, e.date, e.location, a.email AS admin_email
+        SELECT ve.id, e.name AS event_name, ve.status AS type, e.date, e.location, a.email AS admin_email,
+          a.first_name AS admin_first_name, a.middle_name AS admin_middle_name, a.last_name AS admin_last_name, e.description
         FROM volunteer_event ve
         JOIN event e ON ve.event_id = e.id
         JOIN admin a ON e.admin_id = a.id
