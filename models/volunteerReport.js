f11160decf40dc9b7ffd20cbf500540ab511048a
longt2407 <longt2407@gmail.com>
2025-08-04 13:10:30 -0500

    fix volunteer report api

--- models/volunteerReport.js
+++ models/volunteerReport.js
@@ -13,11 +13,11 @@ export const getVolunteerReport = async (key = 'volunteer_id', order = 'asc') =>
                v.first_name AS volunteer_first_name,
                v.middle_name AS volunteer_middle_name,
                v.last_name AS volunteer_last_name,
-               COUNT(CASE WHEN vh.type = 'assigned' THEN 1 END) AS total_assigned_event,
-               COUNT(CASE WHEN vh.type = 'participated' THEN 1 END) AS total_participated_event,
-               COUNT(CASE WHEN vh.type = 'no-show' THEN 1 END) AS total_no_show_event
+               COUNT(CASE WHEN ve.status = 0 THEN 1 END) AS total_assigned_event,
+               COUNT(CASE WHEN ve.status = 1 THEN 1 END) AS total_participated_event,
+               COUNT(CASE WHEN ve.status = 2 THEN 1 END) AS total_no_show_event
            FROM volunteer v
-                    LEFT JOIN volunteer_history vh ON v.id = vh.volunteer_id
+                    LEFT JOIN volunteer_event ve ON v.id = ve.volunteer_id
            GROUP BY v.id, v.email, v.first_name, v.middle_name, v.last_name
            ORDER BY ${orderByColumn} ${order.toUpperCase()}
        `);
@@ -41,10 +41,10 @@ export const getVolunteerReportById = async (volunteerId) => {
                e.id AS event_id,
                e.name AS event_name,
                e.date AS event_date,
-               vh.type AS participation_type
+               ve.status AS participation_type
            FROM volunteer v
-                    JOIN volunteer_history vh ON v.id = vh.volunteer_id
-                    JOIN event e ON vh.event_id = e.id
+                    JOIN volunteer_event ve ON v.id = ve.volunteer_id
+                    JOIN event e ON ve.event_id = e.id
            WHERE v.id = ?
            ORDER BY e.date DESC
        `, [volunteerId]);
